3ae4afdab1f08fe9922a239830e2a75d2bcc60e9
suman bando <sumanbando31122000@gmail.com>
2024-11-26 23:24:39 +0530

    profileimage completed

--- src/profile.jsx
+++ src/profile.jsx
@@ -18,7 +18,7 @@ const ProfilePage = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(`${apiUrl}/user`, {
+        const response = await axios.get(`/api/user`, {
           headers: {
             "Authorization": "Bearer " + sessionStorage.getItem('accessToken')
           }
@@ -32,7 +32,7 @@ const ProfilePage = () => {
     fetchData();  
   }, []);  
 
-  console.log(data);
+  console.log(data.profileimage);
   const user = {
     name: `${data.user}`,
     image: `${data.profileimage}`,
