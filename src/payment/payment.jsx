8de94b686830848faf4cbfde438558508ba5c013
suman bando <sumanbando31122000@gmail.com>
2024-12-07 00:39:39 +0530

    payment gateway

--- src/payment/payment.jsx
+++ src/payment/payment.jsx
@@ -1,4 +1,5 @@
-
+import { useState,useEffect } from "react";
+import axios from "axios";
 const key = import.meta.env.RAZORPAY_KEY_ID;
     const loadRazorpayScript = () => {
         return new Promise((resolve) => {
